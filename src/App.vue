1d38fdb05962307581fba8855dfd05f3277368ce
remcostoeten <remco@tickles.nl>
2021-07-31 02:47:05 +0200

    Worked on alot of scss.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/calculator">Calculator</router-link>
+  <div class="header">
+    <div class="header__menu">
+      <div class="menu__logo">
+        <img style="height: 60px;" alt="Vue logo" src="/images/logo.png">
+      </div>
+      <ul>
+        <li>
+          <router-link to="/"><span>Home</span></router-link>
+        </li>
+        <li>
+          <router-link to="/calculator"><span>Calculator</span></router-link>
+        </li>
+        <li class="login action primary smooth-animation">
+          <router-link to="/login"><span>Login</span></router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="inner">
+      <h2>Crypto portfolio progress</h2>
+      <div class="header__background">
+        <div class="overlay"></div>
+      </div>
+    </div>
   </div>
   <router-view/>
 </template>
