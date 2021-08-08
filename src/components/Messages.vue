4ecc6de152c460f26898e22c2eb32ba0a13e5717
remcostoeten <remco@tickles.nl>
2021-08-08 13:04:01 +0200

    Add front-end authentication messages/alerts on login page.

---
+++ src/components/Messages.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="message" v-for="message in messages">{{ message }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    messages: {
+      type: Array,
+      default: []
+    }
+  }
+}
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,39 +1,55 @@
 <template>
   <form @submit.prevent="submit">
+    <messages :messages="messages"></messages>
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-    <input v-model="data.email" type="email" class="form-control" placeholder="Email" required>
-
-    <input v-model="data.password" type="password" class="form-control" placeholder="Password" required>
-
+    <input v-model="email" type="email" class="form-control" placeholder="Email" required>
+    <input v-model="password" type="password" class="form-control" placeholder="Password" required>
     <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
   </form>
+
+  <div class="message" v-for="message in messages">{{ message }}</div>
+
 </template>
 
 <script lang="ts">
-import {reactive} from 'vue';
-import {useRouter} from "vue-router";
+import Messages from '@/components/Messages.vue';
 
 export default {
-  name: "Login",
-  setup() {
-    const data = reactive({
+  name: 'Login',
+  components: {
+    Messages
+  },
+  data () {
+    return {
+      messages: [],
       email: '',
       password: ''
-    });
-    const router = useRouter();
-    const submit = async () => {
-      await fetch('http://localhost:8000/api/login', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        credentials: 'include',
-        body: JSON.stringify(data)
-      });
-      await router.push('/');
     }
-    return {
-      data,
-      submit
+  },
+  methods: {
+    submit () {
+      const login = async () => {
+        const response = await fetch('http://localhost:8000/api/login', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          credentials: 'include',
+          body: JSON.stringify({
+            email: this.email,
+            password: this.password
+          })
+        });
+        return await response.json();
+      }
+
+      login().then(response => {
+        this.messages = [response.message];
+
+        if (response.ok) {
+          this.$router.push('/');
+        }
+      }).catch(error => {
+        console.log(error);
+      })
     }
   }
 }
